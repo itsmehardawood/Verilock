a1d2326f72f754a7ef822a85a710dd3f59ad9f16
Haris Mohammad <codelignitor@users.noreply.github.com>
2025-10-03 12:09:31 +0500

    build fixed

--- src/app/customer/profiles/instagram/page.js
+++ src/app/customer/profiles/instagram/page.js
@@ -1,6 +1,7 @@
 
+import Facebook from '@/app/components/customer/facebook'
 import React from 'react'
-import Facebook from './../../components/customer/facebook';
+// import Facebook from './../../components/customer/facebook';
 
 export default function facebookPage  () {
   return (
