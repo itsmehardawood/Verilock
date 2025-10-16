89404cf07ac5275830e6eaf1dd822f0ea2146215
codelignitor <harriz789@gmail.com>
2025-10-16 16:14:30 +0500

    Firebase integration

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import LoginPage from './login/page'
-import TestLogin from './test-login/page'
-import TestSignup from './test-signup/page'
+// import TestLogin from './test-login/page'
+// import TestSignup from './test-signup/page'
 
 const Page = () => {
   return (
     <div>
-      <TestSignup/>
+      {/* <TestSignup/> */}
       {/* <TestLogin/> */}
+      <LoginPage/>
     </div>
   )
 }
